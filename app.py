05f278584d9fd973561e08b21c962c83cc0dd1ae
chandni.soni <chandnisoni.mail@gmail.com>
2020-11-03 21:59:02 -0500

    backend improvements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from weather_backend import weather_city, clothes
+from weather_backend import get_weather, get_attire
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Secret-key"
@@ -14,12 +14,12 @@ def index():
 def dress():
     city_name = request.form.get("city_name")
     app.logger.info(f'city name={city_name}')
-    result = weather_city(city_name)
+    result = get_weather(city_name)
     wind = result["wind_speed"]
     temp = result["temperature"]
     feels = result["feels_temperature"]
     description = result["weather_description"]
-    message = str(clothes(city_name))
+    message = str(get_attire(result))
     return render_template("dress.html", wind=wind, cityname=city_name, message=message, temp=temp, feels_temperature=feels, weather_description=description)
 
 
